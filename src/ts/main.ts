a66e3fed0f3470daeb7534333f59efa8f5339da7
Junita Berglin <junita.berglin@gmail.com>
2023-02-17 10:46:39 +0100

    structure change

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,69 +1,78 @@
-// import { contact } from "./modules/pages/contact";
-// import { startpage } from "./modules/pages/startpage";
+// import { work } from "../ts/modules/pages/work";
+// import { projects } from "./modules/projectList";
+// import { Projects } from "./modules/Projects";
 
-import { projects } from "./modules/projectList";
-import { Projects } from "./modules/Projects";
+// work(projects);
 
-const projectOnDisplay: HTMLElement = document.createElement("section");
+// import { projects } from "./modules/projectList";
+// import { Projects } from "./modules/Projects";
 
-projectOnDisplay.classList.add("projectOnDisplay");
+// const projectOnDisplay: HTMLElement =
+//   document.createElement("section");
 
-let projectsOpen = false;
+// projectOnDisplay.classList.add("projectOnDisplay");
 
-function work(projectItems: Projects[]) {
-  projectsOpen = true;
-  document.getElementById("portfolioIcon")?.classList.add("open");
-  projectOnDisplay.innerHTML = "";
+// let projectsOpen = false;
 
-  for (let i = 0; i < projectItems.length; i++) {
-    let projectContainer: HTMLDivElement = document.getElementById(
-      "workWrapper"
-    ) as HTMLDivElement;
-    /* let projectMain: HTMLDivElement = document.getElementById(
-      "mainWrapper"
-    ) as HTMLDivElement; */
-    let projectImage: HTMLImageElement = document.createElement("img");
-    let projectName: HTMLHeadingElement = document.createElement("h3");
-    let projectDescription: HTMLParagraphElement = document.createElement("p");
-    // let link = document.createElement("a");
-    let projectButton: HTMLButtonElement = document.createElement("button");
+// function work(projectItems: Projects[]) {
+//   projectsOpen = true;
+//   document.getElementById("portfolioIcon")?.classList.add("open");
+//   projectOnDisplay.innerHTML = "";
 
-    projectContainer.classList.add("project");
-    projectImage.classList.add("project__image");
-    projectName.classList.add("project__name");
-    projectDescription.classList.add("project__description");
-    projectButton.classList.add("project__button");
+//   for (let i = 0; i < projectItems.length; i++) {
+//     let projectContainer: HTMLDivElement = document.getElementById(
+//       "workWrapper"
+//     ) as HTMLDivElement;
+//     let projectImage: HTMLImageElement =
+//       document.createElement("img");
+//     let projectName: HTMLHeadingElement =
+//       document.createElement("h3");
+//     let projectDescription: HTMLParagraphElement =
+//       document.createElement("p");
+//     // let link = document.createElement("a");
+//     let projectButton: HTMLButtonElement =
+//       document.createElement("button");
 
-    projectImage.src = projectItems[i].image;
-    projectImage.alt = projectItems[i].name;
-    projectName.innerHTML = projectItems[i].name;
-    projectDescription.innerHTML = projectItems[i].describtion;
-    projectButton.innerHTML = "Testa sidan!";
+//     projectContainer.classList.add("project");
+//     projectImage.classList.add("project__image");
+//     projectName.classList.add("project__name");
+//     projectDescription.classList.add("project__description");
+//     projectButton.classList.add("project__button");
 
-    // link.setAttribute("target", "new");
-    // link.appendChild(projectImage);
+//     projectImage.src = projectItems[i].image;
+//     projectImage.alt = projectItems[i].name;
+//     projectName.innerHTML = projectItems[i].name;
+//     projectDescription.innerHTML = projectItems[i].describtion;
+//     projectButton.innerHTML = "Testa sidan!";
 
-    projectImage.addEventListener("click", () => {
-      document.location.href = "https://junitaberglin.se/todo/";
-    });
+//     // link.setAttribute("target", "new");
+//     // link.appendChild(projectImage);
 
-    projectContainer.appendChild(projectName);
-    projectContainer.appendChild(projectImage);
-    projectContainer.appendChild(projectDescription);
-    projectContainer.appendChild(projectButton);
-  }
-}
-let projectClick = document.getElementById("projectClick") as HTMLDivElement;
+//     projectImage.addEventListener("click", () => {
+//       document.location.href = "https://junitaberglin.se/todo/";
+//     });
 
-projectClick.addEventListener("click", () => {
-  if (!projectsOpen) {
-    work(projects);
-  } else {
-    let projectContainer: HTMLDivElement = document.getElementById(
-      "workWrapper"
-    ) as HTMLDivElement;
-    projectContainer.innerHTML = "";
-    projectsOpen = false;
-    document.getElementById("portfolioIcon")?.classList.remove("open");
-  }
-});
+//     projectContainer.appendChild(projectName);
+//     projectContainer.appendChild(projectImage);
+//     projectContainer.appendChild(projectDescription);
+//     projectContainer.appendChild(projectButton);
+//   }
+// }
+// let projectClick = document.getElementById(
+//   "projectClick"
+// ) as HTMLDivElement;
+
+// projectClick.addEventListener("click", () => {
+//   if (!projectsOpen) {
+//     work(projects);
+//   } else {
+//     let projectContainer: HTMLDivElement = document.getElementById(
+//       "workWrapper"
+//     ) as HTMLDivElement;
+//     projectContainer.innerHTML = "";
+//     projectsOpen = false;
+//     document
+//       .getElementById("portfolioIcon")
+//       ?.classList.remove("open");
+//   }
+// });
--- src/ts/modules/pages/work.ts
+++ src/ts/modules/pages/work.ts
@@ -1,50 +1,76 @@
-import { projects } from "../projectList";
+// import { contact } from "./modules/pages/contact";
+// import { startpage } from "./modules/pages/startpage";
 
-export function work() {
-  const workPageContainer: HTMLDivElement =
-    document.createElement("div");
-  const workPageTitle: HTMLParagraphElement =
-    document.createElement("p");
-  workPageContainer.setAttribute("id", "work");
-  workPageContainer.classList.add("work");
-  workPageTitle.classList.add("work__title");
-  workPageTitle.innerHTML = "#work";
-  workPageContainer.appendChild(workPageTitle);
-  document.body.appendChild(workPageContainer);
+import { projects } from "../../modules/projectList";
+import { Projects } from "../../modules/Projects";
 
-  projects.forEach((project) => {
-    const projectContainer: HTMLDivElement =
-      document.createElement("div");
-    const projectTitle: HTMLParagraphElement =
+const projectOnDisplay: HTMLElement =
+  document.createElement("section");
+
+projectOnDisplay.classList.add("projectOnDisplay");
+
+let projectsOpen = false;
+
+export function work(projectItems: Projects[]) {
+  projectsOpen = true;
+  document.getElementById("portfolioIcon")?.classList.add("open");
+  projectOnDisplay.innerHTML = "";
+
+  for (let i = 0; i < projectItems.length; i++) {
+    let projectContainer: HTMLDivElement = document.getElementById(
+      "workWrapper"
+    ) as HTMLDivElement;
+    let projectImage: HTMLImageElement =
+      document.createElement("img");
+    let projectName: HTMLHeadingElement =
+      document.createElement("h3");
+    let projectDescription: HTMLParagraphElement =
       document.createElement("p");
-    const projectImageDiv: HTMLDivElement =
-      document.createElement("div");
-    const projectMobileImage: HTMLImageElement =
-      document.createElement("img");
-    const projectDesktopImage: HTMLImageElement =
-      document.createElement("img");
-    const projectDescription: HTMLSpanElement =
-      document.createElement("span");
+    // let link = document.createElement("a");
+    let projectButton: HTMLButtonElement =
+      document.createElement("button");
 
     projectContainer.classList.add("project");
-    projectTitle.classList.add("project__title");
-    projectImageDiv.classList.add("project__imgcontainer");
-    projectMobileImage.classList.add("project__img--mobile");
-    projectDesktopImage.classList.add("project__img--desktop");
-    projectDescription.classList.add("project__describe");
+    projectImage.classList.add("project__image");
+    projectName.classList.add("project__name");
+    projectDescription.classList.add("project__description");
+    projectButton.classList.add("project__button");
 
-    projectTitle.innerHTML = project.name;
-    projectMobileImage.alt = project.image + " mobile";
-    projectDescription.innerHTML = project.describtion;
+    projectImage.src = projectItems[i].image;
+    projectImage.alt = projectItems[i].name;
+    projectName.innerHTML = projectItems[i].name;
+    projectDescription.innerHTML = projectItems[i].describtion;
+    projectButton.innerHTML = "Ta en närmare titt!";
 
-    projectContainer.appendChild(projectTitle);
-    projectImageDiv.appendChild(projectDesktopImage);
-    projectImageDiv.appendChild(projectMobileImage);
-    projectContainer.appendChild(projectImageDiv);
+    // link.setAttribute("target", "new");
+    // link.appendChild(projectImage);
+
+    projectImage.addEventListener("click", () => {
+      document.location.href =
+        "https://github.com/JunitaBerglin?tab=repositories";
+    });
+
+    projectContainer.appendChild(projectName);
+    projectContainer.appendChild(projectImage);
     projectContainer.appendChild(projectDescription);
-
-    workPageContainer.appendChild(projectContainer);
-  });
-
-  document.body.appendChild(workPageContainer);
+    projectContainer.appendChild(projectButton);
+  }
 }
+let projectClick = document.getElementById(
+  "projectClick"
+) as HTMLDivElement;
+
+projectClick.addEventListener("click", () => {
+  if (!projectsOpen) {
+    work(projects);
+  } else {
+    let projectContainer: HTMLDivElement = document.getElementById(
+      "workWrapper"
+    ) as HTMLDivElement;
+    projectContainer.innerHTML = "";
+    projectsOpen = false;
+    document
+      .getElementById("portfolioIcon")
+      ?.classList.remove("open");
+  }
+});
